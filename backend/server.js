796d1e275f9e9b0d0c7179959ec6cab73700e69a
Vedika Jadhav <vedikajadhav032006@gmail.com>
2026-05-30 13:17:14 +0000

    fix: allow all CORS origins

--- backend/server.js
+++ backend/server.js
@@ -5,22 +5,16 @@ require('dotenv').config();
 
 const app = express();
 
-// Middleware
-app.use(cors({
-  origin: 'https://devboard-lovat-nu.vercel.app'
-}));
+app.use(cors());
 app.use(express.json());
 
-// Routes
 const taskRoutes = require('./routes/tasks');
 app.use('/api/tasks', taskRoutes);
 
-// MongoDB Connect
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB Connected'))
   .catch(err => console.log('❌ Error:', err));
 
-// Server Start
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
